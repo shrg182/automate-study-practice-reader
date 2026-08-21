c15e0ea70f5426b41823bc6c01b1793ce4445db2
Ruixing Shi <ruixing.shi3@gmail.com>
2026-08-21 00:53:38 -0400

    feat: expose Chinese war reference links

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "reading-room-v10";
+const VERSION = "reading-room-v11";
 const CORE_CACHE = `${VERSION}-core`;
 const ARTICLE_CACHE = `${VERSION}-articles`;
 const root = new URL("./", self.registration.scope);
