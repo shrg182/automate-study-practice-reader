0253e0a4834ce077c6e286e1c59566c4e1460a12
Ruixing Shi <ruixing.shi3@gmail.com>
2026-08-20 22:49:02 -0400

    fix: restore reading-mode book button contrast

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "reading-room-v6";
+const VERSION = "reading-room-v7";
 const CORE_CACHE = `${VERSION}-core`;
 const ARTICLE_CACHE = `${VERSION}-articles`;
 const root = new URL("./", self.registration.scope);
