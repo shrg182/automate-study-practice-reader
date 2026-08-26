6c59f709b2cc7ec5d11d2f61bbe8ec858def1a13
Ruixing Shi <ruixing.shi3@gmail.com>
2026-08-26 15:01:03 -0400

    Make book dropdowns script independent

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "reading-room-v43";
+const VERSION = "reading-room-v44";
 const CORE_CACHE = `${VERSION}-core`;
 const ARTICLE_CACHE = `${VERSION}-articles`;
 const root = new URL("./", self.registration.scope);
