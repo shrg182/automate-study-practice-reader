6f61701ae6b6f22bd9928dee886ff7d09e44b761
Ruixing Shi <ruixing.shi3@gmail.com>
2026-08-26 00:08:28 -0400

    Collapse Marxist Classics book contents by default

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "reading-room-v34";
+const VERSION = "reading-room-v35";
 const CORE_CACHE = `${VERSION}-core`;
 const ARTICLE_CACHE = `${VERSION}-articles`;
 const root = new URL("./", self.registration.scope);
