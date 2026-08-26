f851eeacb9f964e285d50b36e68aacfc345d5f37
Ruixing Shi <ruixing.shi3@gmail.com>
2026-08-26 00:24:46 -0400

    Fix Marxist book contents toggles

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "reading-room-v37";
+const VERSION = "reading-room-v38";
 const CORE_CACHE = `${VERSION}-core`;
 const ARTICLE_CACHE = `${VERSION}-articles`;
 const root = new URL("./", self.registration.scope);
