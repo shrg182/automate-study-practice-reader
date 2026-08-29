bcd62fe613900e8d66bdeaa57c5a7885a03ceeeb
Ruixing Shi <ruixing.shi3@gmail.com>
2026-08-29 15:26:22 -0400

    Refine compact home navigation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "reading-room-v70";
+const VERSION = "reading-room-v71";
 const CORE_CACHE = `${VERSION}-core`;
 const ARTICLE_CACHE = `${VERSION}-articles`;
 const root = new URL("./", self.registration.scope);
