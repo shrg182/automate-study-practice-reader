618a001d71e2a5de93df9f0bfd07591a7f5b009a
Ruixing Shi <ruixing.shi3@gmail.com>
2026-08-31 18:32:16 -0400

    Fix Chinese-first pane placement

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "reading-room-v86";
+const VERSION = "reading-room-v87";
 const CORE_CACHE = `${VERSION}-core`;
 const ARTICLE_CACHE = `${VERSION}-articles`;
 const root = new URL("./", self.registration.scope);
