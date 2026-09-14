8a82a5eb4491d52cd706211f88f76496df8889ca
shrg182 <ruixing.shi3@gmail.com>
2026-09-15 00:58:58 +0800

    Restore collection selection links

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "reading-room-v107";
+const VERSION = "reading-room-v108";
 const CORE_CACHE = `${VERSION}-core`;
 const ARTICLE_CACHE = `${VERSION}-articles`;
 const root = new URL("./", self.registration.scope);
